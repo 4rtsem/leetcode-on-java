a62b0b9ced5fe421d890c0bcc166a0aa4b7cc532
4rtsem <4rtsem@gmail.com>
2024-05-26 17:12:52 +0300

    [88 MergeSortedArray] Add should_merge_example_2() & should_merge_example_3() tests

--- src/test/java/uk/sauch/topinterview150/MergeSortedArray/SolutionTest.java
+++ src/test/java/uk/sauch/topinterview150/MergeSortedArray/SolutionTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
 
-
     @Test
     void should_merge_example_1() {
         // given
@@ -16,6 +15,38 @@ class SolutionTest {
         final var n = 3;
 
         final var expected = new int[]{1, 2, 2, 3, 5, 6};
+
+        // when
+        new Solution().merge(nums1, m, nums2, n);
+
+        // then
+        assertThat(nums1).isEqualTo(expected);
+    }
+
+    @Test
+    void should_merge_example_2() {
+        final var nums1 = new int[]{1};
+        final var m = 1;
+        final var nums2 = new int[]{};
+        final var n = 0;
+
+        final var expected = new int[]{1};
+
+        // when
+        new Solution().merge(nums1, m, nums2, n);
+
+        // then
+        assertThat(nums1).isEqualTo(expected);
+    }
+
+    @Test
+    void should_merge_example_3() {
+        final var nums1 = new int[]{0};
+        final var m = 0;
+        final var nums2 = new int[]{1};
+        final var n = 1;
+
+        final var expected = new int[]{1};
 
         // when
         new Solution().merge(nums1, m, nums2, n);
