b235870ac8e27e98543158d445fad8f27b215246
4rtsem <4rtsem@gmail.com>
2024-05-28 14:06:11 +0300

    [190 ReverseBits] Example 2 test ; Java SDK Solution

--- src/main/java/uk/sauch/topinterview150/ReverseBits/Solution.java
+++ src/main/java/uk/sauch/topinterview150/ReverseBits/Solution.java
@@ -2,6 +2,13 @@ package uk.sauch.topinterview150.ReverseBits;
 
 public class Solution {
     public int reverseBits(int n) {
-        throw new RuntimeException("Not implemented");
+        final var binaryString = Integer.toBinaryString(n);
+        final var offset = 32 - binaryString.length();
+        final var sb = new StringBuilder();
+
+        sb.append("0".repeat(offset));
+        sb.append(binaryString);
+
+        return Integer.parseUnsignedInt(sb.reverse().toString(), 2);
     }
 }
--- src/test/java/uk/sauch/topinterview150/ReverseBits/SolutionTest.java
+++ src/test/java/uk/sauch/topinterview150/ReverseBits/SolutionTest.java
@@ -20,4 +20,43 @@ class SolutionTest {
         // then
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    void should_reverse_bits_example_2() {
+        // given
+        // 11111111111111111111111111111101
+        final var input = -3;
+        // 10111111111111111111111111111111
+        final var expected = -1073741825;
+
+        // when
+        final var actual = new Solution().reverseBits(input);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE - 1);
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE - 1));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE + 1));
+
+        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE - 1));
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+        System.out.println(Integer.MIN_VALUE + 1);
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE + 1));
+
+        // Math.pow(2,31) - 1          ; 01111111111111111111111111111110
+        // Math.pow(2,31)              ; 01111111111111111111111111111111
+        // (Math.pow(2,31) + 1) * (-1) ; 10000000000000000000000000000000
+
+        // Math.pow(2,31)              ; 01111111111111111111111111111111
+        // (Math.pow(2,31) + 1) * (-1) ; 10000000000000000000000000000000
+        // Math.pow(2,31) * (-1)       ; 10000000000000000000000000000001
+    }
 }
