e4a6c80a296b46066ca2073555ed3a619b2ffd38
4rtsem <4rtsem@gmail.com>
2024-05-28 16:04:43 +0300

    [190 ReverseBits] Remove offset variable

--- src/main/java/uk/sauch/topinterview150/ReverseBits/Solution.java
+++ src/main/java/uk/sauch/topinterview150/ReverseBits/Solution.java
@@ -3,10 +3,9 @@ package uk.sauch.topinterview150.ReverseBits;
 public class Solution {
     public int reverseBits(int n) {
         final var binaryString = Integer.toBinaryString(n);
-        final var offset = 32 - binaryString.length();
         final var sb = new StringBuilder();
 
-        sb.append("0".repeat(offset));
+        sb.append("0".repeat(Integer.numberOfLeadingZeros(n)));
         sb.append(binaryString);
 
         return Integer.parseUnsignedInt(sb.reverse().toString(), 2);
